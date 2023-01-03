828ad13d6ce55de84579326b71b5c2e9ae093447
Prajwal-S-Yallur <63166980+Prajwal-S-Yallur@users.noreply.github.com>
2023-01-03 09:38:47 +0530

    first commit

---
+++ FinanceApp.py
@@ -0,0 +1,45 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+# Import from your custom module
+from FinanceDB.SetupDB import Finance
+
+# Connect to the database
+engine = create_engine("sqlite:///FinanceDB//finance_database.db")
+
+# Basic Flask imports
+from flask import Flask, request, redirect, url_for, render_template
+
+# Initialize Flask
+app = Flask(__name__)
+
+
+@app.route("/finance")
+def create_transaction():
+    return render_template("create_transaction.html")
+
+
+@app.route("/create_transaction", methods = ["POST", "GET"])
+def createTransaction():
+    
+    if request.method == "POST":
+        # Get the data from the form and placed into a variable. 
+        input_data = request.form
+        
+        # A new session will have to be created in every function
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        
+        # create a new transaction and enter it into the database
+        new_transaction = Finance(transation_name = input_data["transation_name"], amount_spent = input_data["amount_spent"])
+        session.add(new_transaction)
+        session.commit()
+        session.close()
+        return redirect(url_for("create_transaction"))
+    
+    else:
+        return redirect(url_for("create_transaction"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ FinanceDB/SetupDB.py
@@ -0,0 +1,38 @@
+# Imports
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.sql import func
+import datetime
+import pytz
+
+# Pass a declarative base and create a object to correspond with a table in the database
+Base = declarative_base()
+    
+class Finance(Base):
+    
+    __tablename__ = 'finance'
+    transaction_id = Column(Integer, primary_key=True)
+    # transaction_date_time = Column(DateTime(timezone=True), default=pytz.timezone("Asia/Kolkata").localize(datetime.datetime.now()), nullable=False)
+    transaction_name = Column(String(250), nullable=False)
+    # product_details = Column(String(250), nullable=False)
+    # product_seller = Column(String(250), nullable=False)
+    # expenditure_category = Column(String(250), nullable=False)
+    # expenditure_sub_category = Column(String(250), nullable=False)
+    amount_spent = Column(Float, nullable=False)
+
+
+# Connect to and create the movie table
+engine = create_engine("sqlite:///FinanceDB//finance_database.db")
+# engine = create_engine("sqlite:///movies_database.db")
+Base.metadata.create_all(engine)
+
+# # Start a session with the database
+# Session = sessionmaker(bind=engine)
+# session = Session()
+# # create a new movie and enter it into the database
+# new_transaction = Finance(transaction_name = "AI and Machine Learning for Coders", amount_spent = 1500)
+# # new_transaction = Finance(transaction_name = "AI and Machine Learning for Coders", product_details = "AI and Machine Learning for Coders by Lorence Morony", product_seller = "Amazon", expenditure_category = "Book", expenditure_sub_category = "Technology - AI / ML / DL / DS", amount_spent = 1500)
+# session.add(new_transaction)
+# session.commit()
