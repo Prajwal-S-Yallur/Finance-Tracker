fc9402c7a62ed234e588585d3c6feee664f8e2eb
Prajwal-S-Yallur <63166980+Prajwal-S-Yallur@users.noreply.github.com>
2023-01-04 09:37:29 +0530

    adding expenditure category and sub-category fields

--- FinanceApp.py
+++ FinanceApp.py
@@ -1,16 +1,14 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-
 import datetime
-
+import pytz
+# Basic Flask imports
+from flask import Flask, redirect, render_template, request, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 # Import from your custom module
 from FinanceDB.SetupDB import Finance
 
 # Connect to the database
 engine = create_engine("sqlite:///FinanceDB//finance_database.db")
-
-# Basic Flask imports
-from flask import Flask, request, redirect, url_for, render_template
 
 # Initialize Flask
 app = Flask(__name__)
@@ -21,27 +19,40 @@ def create_transaction():
     return render_template("create_transaction.html")
 
 
-@app.route("/create_transaction", methods = ["POST", "GET"])
+@app.route("/create_transaction", methods=["POST", "GET"])
 def createTransaction():
-    
+
     if request.method == "POST":
-        # Get the data from the form and placed into a variable. 
+        # Get the data from the form and placed into a variable.
         input_data = request.form
-        
+
         # A new session will have to be created in every function
         Session = sessionmaker(bind=engine)
         session = Session()
-        
+
         # create a new transaction and enter it into the database
-        new_transaction = Finance(transaction_date_time = datetime.datetime.strptime(input_data["transaction_date_time"], '%Y-%m-%dT%H:%M'), transaction_name = input_data["transaction_name"], product_details = input_data["product_details"], product_seller = input_data["product_seller"], amount_spent = input_data["amount_spent"])
+        new_transaction = Finance(
+            transaction_date_time=datetime.datetime.strptime(
+                input_data["transaction_date_time"], "%Y-%m-%dT%H:%M"
+            )
+            if input_data["transaction_date_time"]
+            else pytz.timezone("Asia/Kolkata").localize(datetime.datetime.now()),
+            transaction_name=input_data["transaction_name"],
+            product_details=input_data["product_details"],
+            product_seller=input_data["product_seller"],
+            expenditure_category=input_data["expenditure_category"],
+            expenditure_sub_category=input_data["expenditure_sub_category"],
+            amount_spent=input_data["amount_spent"],
+        )
+
         session.add(new_transaction)
         session.commit()
         session.close()
         return redirect(url_for("create_transaction"))
-    
+
     else:
         return redirect(url_for("create_transaction"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
